dba7097d3fb4688025a2d5fcdd11fa72e5431128
Asger Schultz <asger.s@protonmail.com>
2021-11-05 22:55:38 +0000

    Fix retina for flask

--- wheres-waldo/retina_model.py
+++ wheres-waldo/retina_model.py
@@ -7,6 +7,9 @@ from keras_retinanet.utils.image import read_image_bgr, preprocess_image, resize
 
 PATH = "weights.h5"
 
+global graph
+graph = tf.get_default_graph()
+
 def setup_retina_model():
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
@@ -15,16 +18,17 @@ def setup_retina_model():
     return models.load_model(PATH, backbone_name="resnet50")
 
 def retina_predict(model, img: np.ndarray) -> tuple[int, int]:
-
-    image = preprocess_image(img)
-    image, scale = resize_image(image, min_side=1800, max_side=3000)
-    boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
-    boxes /= scale
-    xmin, xmax, ymin, ymax = boxes[0][0]
-    print(scores[0][0])
-    x = int(round((xmin+xmax)/2))
-    y = int(round((ymin+ymax)/2))
-    return x, y
+    with graph.as_default():
+        image = preprocess_image(img)
+        #image, scale = resize_image(image, min_side=1800, max_side=3000)
+        image, scale = resize_image(image, min_side=1500, max_side=3000)
+        boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
+        boxes /= scale
+        xmin, xmax, ymin, ymax = boxes[0][0]
+        print(scores[0][0])
+        x = int(round((xmin+xmax)/2))
+        y = int(round((ymin+ymax)/2))
+        return x, y
 
 if __name__ == "__main__":
     from PIL import Image
