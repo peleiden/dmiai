b57fa1c4cc0113d67d7dfd1afa9fb0e692d437e3
Asger Schultz <asger.s@protonmail.com>
2021-11-06 00:01:53 +0000

    Fix C++ model

--- wheres-waldo/cpp_model.py
+++ wheres-waldo/cpp_model.py
@@ -4,19 +4,19 @@ from wallyfinder import WallyFinder
 def setup_cpp_model():
     return WallyFinder()
 
-def predict_cpp_model(model, img):
+def cpp_predict(model, img):
     results = model(img)
     if not results:
         return 0, 0
     best = sorted(results, key=lambda x: x["confidence"])[-1]
     print(best["confidence"])
-    x = (best["xmax"] + best["xin"]) / 2
-    y = (best["ymax"] + best["ymin"]) / 3
+    x = int(round((best["xmax"] + best["xmin"]) / 2))
+    y = int(round((best["ymax"] + best["ymin"]) / 2))
     return x, y
 
 if __name__ == "__main__":
     from PIL import Image
     m = setup_cpp_model()
     print(
-        predict_cpp_model(m, np.array(Image.open("data/waldo/1_1_1.jpg")))
+        cpp_predictt(m, np.array(Image.open("data/waldo/1_1_1.jpg")))
     )
