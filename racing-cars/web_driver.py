ff2a437af216574d477636eba4aed6cee13bfb34
Asger Schultz <asger.s@protonmail.com>
2021-11-06 21:13:24 +0100

    Fixies

--- racing-cars/web_driver.py
+++ racing-cars/web_driver.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import sys
 import time
 
@@ -13,18 +14,24 @@ HOST = "127.0.0.1"
 
 def start_driver(port: int, seed: int=0):
     print("Starting Selenium server ...")
-    driver = webdriver.Firefox()
+    driver = webdriver.Firefox(os.path.split(shutil.which("firefox"))[0])
+    print(driver)
     driver.get(SERVICE)
+    print("driver")
     # Fill in data
     for _id, data in zip(("random-seed", "api-protocol", "api-host", "api-port"), (seed, "http", HOST, port)):
+        print(_id)
         elem = driver.find_element_by_id(_id + "-input-field")
         elem.send_keys(data)
+    print("loop")
     # Enable loop
     driver.find_element_by_id("auto-restart-button").click()
     driver.refresh()
+    print("heals")
     # Perform health check
     driver.find_element_by_id("health-check-button").click()
     time.sleep(2)
+    print("status")
     status = driver.find_element_by_id("status-message").get_attribute("innerHTML")
     assert "Ready" in status, f"Health check failed!, status was {status}"
     # Start the server
