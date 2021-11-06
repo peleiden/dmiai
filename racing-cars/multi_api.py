e5f8c5c6f02b6874c5cfd4b0f2fa88b14dc3ec8d
Søren Winkel Holm <sh@writereader.com>
2021-11-06 20:37:43 +0100

    Fix multi API

--- racing-cars/multi_api.py
+++ racing-cars/multi_api.py
@@ -6,7 +6,7 @@ N = int(sys.argv[1])
 PORT_0 = 6969
 
 for i in range(N):
-    print("Starting process {i}")
+    print(f"Starting process {i}")
     cmd = f"python3 api.py --port {PORT_0 + i} --selenium"
     proc = subprocess.Popen([cmd], shell=True,
              stdin=None, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, close_fds=True)
